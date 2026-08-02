80bb37e046e636b650fd1abb7abf159b557f12f3
leahchristina <lcbcurran@gmail.com>
2026-08-02 22:37:04 +0100

    Analyse hand & congratulate user

--- assignment2-carddraw.py
+++ assignment2-carddraw.py
@@ -1,5 +1,3 @@
-
-
 import requests
 
 SHUFFLE_URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
@@ -31,3 +29,35 @@ cards = draw_data["cards"]
 print("Your 5 cards:\n")
 for card in cards:
     print(f"{card['value']} of {card['suit']}")
+
+# Analyse Hand
+values = [card["value"] for card in cards]
+suits = [card["suit"] for card in cards]
+
+# Pair / Triple
+value_counts = {}
+for value in values:
+    value_counts[value] = value_counts.get(value, 0) + 1
+
+if 3 in value_counts.values():
+    print("\nCongratulations! You drew a triple")
+elif 2 in value_counts.values():
+    print("\nCongratulations! You drew a pair")
+
+# Flush (all same suit)
+if len(set(suits)) == 1:
+    print("Congratulations! You drew a flush")
+
+# Straight
+try:
+    nums = sorted(card_value_num(v) for v in values)
+    is_straight = all(nums[i] + 1 == nums[i + 1] for i in range (4))
+
+    # Ace-high straight check
+    if sorted(nums) == [1, 10, 11, 12, 13]:
+        is_straight = True
+
+    if is_straight:
+        print("Congratulations! You drew a straight")
+except Exception: 
+    pass
